3a000fdc17ded4bea93e492d4dd88718dd8f74cf
suvpatil <suvit.patil@capgemini.com>
2017-04-18 11:41:26 +0530

    Update DataAccessLayer.go

--- DataAccessLayer.go
+++ DataAccessLayer.go
@@ -118,7 +118,7 @@ func PutBankList(stub shim.ChaincodeStubInterface, BankList []string) (bool, err
 	return true, nil
 }
 
-func GetKYCDetails(stub shim.ChaincodeStubInterface, UserId string) (KycData, error) {
+func GetKYCDetails(stub shim.ChaincodeStubInterface, UserId string) (KycData, bool, error) {
 	var KycDataObj KycData
 	var BankList []string
 	var columns []shim.Column
@@ -141,7 +141,7 @@ func GetKYCDetails(stub shim.ChaincodeStubInterface, UserId string) (KycData, er
 
 	BankList, err = GetBankList(stub)
 	if err != nil {
-		return error, err
+		return false, err
 	}
 
 	if KycDataObj.KYC_BANK_NAME == BankList[0] {
